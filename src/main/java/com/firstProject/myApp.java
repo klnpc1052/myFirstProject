03471c2ea34b01833dbbea32c5a40a0d284f3fe0
klnpc1052 <105124325+klnpc1052@users.noreply.github.com>
2026-01-21 07:30:07 +0530

    Update output message in myApp class

--- src/main/java/com/firstProject/myApp.java
+++ src/main/java/com/firstProject/myApp.java
@@ -3,9 +3,10 @@ package com.firstProject;
 public class myApp {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Raajesh");
+		System.out.println("Hello Maven Project is building");
 
 	}
 
 }
+
 
